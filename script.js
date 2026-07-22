1ce563b565a53e9ba34ad2ff2b98476112613ced
glamusalon2022-collab <glamusalon2022@gmail.com>
2026-07-22 23:31:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Glam U Salon');
+// Glass Navbar on Scroll
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+
+    if (header) {
+        if (window.scrollY > 20) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    }
+});
